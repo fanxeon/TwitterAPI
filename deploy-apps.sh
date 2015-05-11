eee4cec6c24c342b8fbc9d0cc49fa26cca34635b
skfmu <skfmu@users.noreply.github.com>
2015-05-11 12:30:25 +0000

    Refactored apps. Removed chirp-monit. Cleaned up vars bug.

--- deploy-apps.sh
+++ deploy-apps.sh
@@ -2,7 +2,8 @@
 #
 # Script to quickly deploy apps without
 # requiring all credentials for provisioning.
-# Deploys apps in `./deploy` directory (symlink to ansible template dir).
+# Deploys apps in `./deploy-templates` directory (symlink to ansible template dir).
+# Add an appropriate config in `./deploy-conf.yml`.
 # Make sure to have team19.pem loaded
 # e.g. `ssh-agent $SHELL && ssh-add team19.pem`.
 
