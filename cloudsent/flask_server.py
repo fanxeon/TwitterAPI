22045a5a42b1508003e120f1797ac07cc9c78945
fanxeon <fanxuanhow@gmail.com>
2015-05-20 07:29:12 +1000

    Update flask_server.py

--- cloudsent/flask_server.py
+++ cloudsent/flask_server.py
@@ -38,6 +38,41 @@ def graph(message):
         response.append({'location' : r.value})    
     return json.dumps(response)
 
+##################################################################
+# NEW DEVELOPMENT                                                #
+##################################################################
+
+# RAW VIEW - Get Particular tweet
+@app.route('/rawtweet/<tweetid>')
+def rawtweet(tweetid):
+    # DB configuration
+    server = couchdb.client.Server('http://localhost:5984/')
+    database = server['twitter']  # Using a smaller db for testing
+    results = database.get(tweetid)
+    return results
+
+
+# TEMPLATES VIEW - Get particular tweet - NOT USING
+@app.route('/viewtweet/<tweetid>')
+def viewtweet(tweetid):
+#results = database.get(tweetid)
+#if results['analytics']['sentiment'] == 'positive' :
+#    return render.result_pos(results)
+#elif results['analytics']['sentiment'] == 'negative' :
+#    return render.result_neg(results)
+#else :
+#    return render_template('result.html', tweetid=tweetid)
+
+# 404 NOT FOUND
+@app.errorhandler(404)
+def page_not_found(error):
+    return "404 NOT FOUND : Your Request cannot be reached"
+
+##################################################################
+# END                                                            #
+##################################################################
+
+
  
 if __name__ == "__main__":
     app.run(debug=True)
